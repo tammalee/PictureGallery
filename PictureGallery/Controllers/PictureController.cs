94a8d1c75a5d7839d7b1907192e73e601028f944
Tammy Valgardson <tammalee@gmail.com>
2020-11-06 20:55:27 -0700

    Create Only logged in users can see the create page. If you are not logged in you are redirected to gallery.

---
+++ PictureGallery/Controllers/PictureController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PictureGallery.Controllers
+{
+    public class PictureController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            IActionResult action;
+            if( User.Identity.IsAuthenticated )
+            {
+                action = View();
+            }
+            else
+            {
+                // if not logged in, redirect to gallery
+                action = RedirectToAction("List");
+            }
+            return action;
+        }
+    }
+}
