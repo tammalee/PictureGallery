db9cc89e904ef1f5bc7b773b4c5662895b0f67c0
Tammy Valgardson <tammalee@gmail.com>
2020-11-07 14:30:31 -0700

    Upload form Added styling to form. Changed form to only upload files. Form submits and saves a file to the wwwroot folder.

--- PictureGallery/Controllers/PictureController.cs
+++ PictureGallery/Controllers/PictureController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,9 +23,10 @@ namespace PictureGallery.Controllers
             return View();
         }
 
-        public IActionResult Create()
+        public IActionResult Create(string title)
         {
             IActionResult action;
+
             if( User.Identity.IsAuthenticated )
             {
                 action = View();
@@ -34,6 +37,34 @@ namespace PictureGallery.Controllers
                 action = RedirectToAction("List");
             }
             return action;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            if( files.Count <= 0)
+            {
+                ViewBag.Error = "You must select a file to upload.";
+                return View("Create");
+            }
+            
+            long size = files.Sum(f => f.Length);
+            var filePaths = new List<string>();
+
+            foreach (var formFile in files)
+            {
+                if( formFile.Length > 0)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/uploads", formFile.FileName);
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            //return View("Create");
+            return Ok(new { count = files.Count, size, filePaths });
         }
     }
 }
